50df5b9e9ebc63c9b5814a15dd50fcc1b74648cf
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-07-30 14:53:20 -0400

    Hard coding the game

--- hangman.py
+++ hangman.py
@@ -1,6 +1,19 @@
-import sys
 
-NAME = input('What is your name?  ')
 
-print(f"Hello {NAME}! Would you like to play a quick game of Hangman?")
+################################################################################################
+#                                             Variables
+################################################################################################
 
+NAME = input('What is your name? ')
+
+print(f"Hello {NAME}!")
+print("Would you like to play a quick game of Hangman? yes or no")
+
+
+
+if  input() == "yes":
+    print("In this Hangman, you will have 7 guesses before you lose the game. Please use only letters of the Alphapet or you will lose that turn. Good Luck!")
+    print(" ")
+    input("____")
+else:
+    print("Have a good day !")
