55e1b07460f0d0d9fbb3f2e536f3ddd5af56cb3c
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-08-01 15:24:42 -0400

    Continued working on the hangman project

--- hangman.py
+++ hangman.py
@@ -14,6 +14,8 @@ print("Would you like to play a quick game of Hangman? yes or no")
 if  input() == "yes":
     print("In this Hangman, you will have 7 guesses before you lose the game. Please use only letters of the Alphapet or you will lose that turn. Good Luck!")
     print(" ")
-    input("____")
+    input("Guess a letter  ")
 else:
     print("Have a good day !")
+
+
