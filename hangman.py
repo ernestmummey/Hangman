fd91c2d499dc6e37971986e9a27256b25986668d
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-08-02 20:29:04 -0400

    Added comments to the program

--- hangman.py
+++ hangman.py
@@ -1,11 +1,11 @@
 
 
-################################################################################################
-#                                             Variables
-################################################################################################
 
+# Variables
 NAME = input('What is your name? ')
 
+
+# Main function for the game
 def main():
     print(f"Hello {NAME}!")
     print("Would you like to play a quick game of Hangman? yes or no")
@@ -13,14 +13,15 @@ def main():
     if  input() == "yes":
         print("In this Hangman, you will have 7 guesses before you lose the game. Please use only letters of the Alphapet or you will lose that turn. Good Luck!")
         print(" ")
+        # helper funtion to offically start the game
         startGame()
     else:
         print("Have a good day !")
 
-
+# helper function to start the guessing of the game
 def startGame():
     input("Pick a letter ")
 
-
+# This will start the the function call of the main function. Python first looks for a module name "main" then starts the program 
 if __name__=="__main__":
     main()
