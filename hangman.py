c8c4e57e0164a2f4e9ed98b264f46bf8581685de
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-08-02 20:15:52 -0400

    created def main, def startgame, name == main

--- hangman.py
+++ hangman.py
@@ -6,16 +6,21 @@
 
 NAME = input('What is your name? ')
 
-print(f"Hello {NAME}!")
-print("Would you like to play a quick game of Hangman? yes or no")
+def main():
+    print(f"Hello {NAME}!")
+    print("Would you like to play a quick game of Hangman? yes or no")
+
+    if  input() == "yes":
+        print("In this Hangman, you will have 7 guesses before you lose the game. Please use only letters of the Alphapet or you will lose that turn. Good Luck!")
+        print(" ")
+        startGame()
+    else:
+        print("Have a good day !")
 
 
-
-if  input() == "yes":
-    print("In this Hangman, you will have 7 guesses before you lose the game. Please use only letters of the Alphapet or you will lose that turn. Good Luck!")
-    print(" ")
-    input("Guess a letter  ")
-else:
-    print("Have a good day !")
+def startGame():
+    input("Pick a letter ")
 
 
+if __name__=="__main__":
+    main()
